1ba42079213a34dbbd310d37452afc336dea7774
xmascu <119685592+xmascu@users.noreply.github.com>
2022-12-07 10:40:58 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,65 @@
-use forensic_rs::prelude::{RegistryReader, ForensicResult, RegValue};
+use forensic_rs::prelude::{RegistryReader, ForensicResult};
 
 fn main() {
-    let _ = exploring_registry().unwrap();
+    let _ = last_visited_pid_mru().unwrap();
+    println!("--------------CAMBIO DE REGISTRO----------------------");
+    let _ = open_save_pid_mru().unwrap();
 }
 
-fn exploring_registry() -> ForensicResult<()> {
+fn last_visited_pid_mru() -> ForensicResult<()> {
     let mut registry  = frnsc_liveregistry_rs::LiveRegistryReader{};
 
     let reg_key = registry.open_key
     (forensic_rs::prelude::RegHiveKey::HkeyCurrentUser,"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\LastVisitedPidlMRU")?; 
     //Abrimos la clave
 
-    let mut reg_values = registry.enumerate_values(reg_key)?; 
-    // Enumeramos todos los valores de esas claves
+    let reg_values = registry.enumerate_values(reg_key)?; 
+    // Enumeramos todos los valores
     
     for top in reg_values {
-        println!("{top}");
-        let value : Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
-        let hex = hex::encode(&value);
-        println!("{:?}", hex);
+        println!("------------ {}", top);
+        if !top.eq("MRUListEx") {
+            let value : Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
+            let vec = vec_u8_to_vec_char(value);
+            let strin: String = vec.iter().collect::<String>();
+            let split: Vec<String> = strin.split("PO :i+00").map(str::to_string).collect();
+            println!("Program used to open / save as: {:?}", &split[0]);
+            println!("Directory where the file was: {:?}", &split[1]);
+        }
     }
     return Ok(());
+}
+
+fn open_save_pid_mru() -> ForensicResult<()> {
+    let mut registry  = frnsc_liveregistry_rs::LiveRegistryReader{};
+
+    let reg_key = registry.open_key
+    (forensic_rs::prelude::RegHiveKey::HkeyCurrentUser,"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSavePidlMRU\\*")?; 
+    //Abrimos la clave
+
+    let reg_values = registry.enumerate_values(reg_key)?; 
+    // Enumeramos todos los valores
+    
+    for top in reg_values {
+        println!("------------ {}", top);
+        let value : Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
+        let vec = vec_u8_to_vec_char(value);
+        let strin: String = vec.iter().collect::<String>();
+        println!("{}", strin);
+    }
+    return Ok(());
+}
+
+fn vec_u8_to_vec_char(value : Vec<u8>) -> Vec<char> {
+
+    let mut vec : Vec<char> = Vec::new();
+
+    for i in value {
+        if i >= 32 && i <= 126 {
+            vec.push(i as char);
+        }    
+    }
+
+    return vec;
 }
 
