165c7f9d29dd107875b81264b67ebbde326ba198
xmascu <119685592+xmascu@users.noreply.github.com>
2022-12-15 13:02:49 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
+use std::{u8,str};
 use forensic_rs::prelude::{RegistryReader, ForensicResult};
 
 fn main() {
     let _ = last_visited_pid_mru().unwrap();
-    println!("--------------CAMBIO DE REGISTRO----------------------");
-    let _ = open_save_pid_mru().unwrap();
 }
 
 fn last_visited_pid_mru() -> ForensicResult<()> {
     let mut registry  = frnsc_liveregistry_rs::LiveRegistryReader{};
 
     let reg_key = registry.open_key
-    (forensic_rs::prelude::RegHiveKey::HkeyCurrentUser,"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\LastVisitedPidlMRU")?; 
+    (forensic_rs::prelude::RegHiveKey::HkeyCurrentUser,"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\LastVisitedPidlMRU")?;
     //Abrimos la clave
 
     let reg_values = registry.enumerate_values(reg_key)?; 
@@ -19,47 +18,71 @@ fn last_visited_pid_mru() -> ForensicResult<()> {
     for top in reg_values {
         println!("------------ {}", top);
         if !top.eq("MRUListEx") {
-            let value : Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
-            let vec = vec_u8_to_vec_char(value);
-            let strin: String = vec.iter().collect::<String>();
-            let split: Vec<String> = strin.split("PO :i+00").map(str::to_string).collect();
-            println!("Program used to open / save as: {:?}", &split[0]);
-            println!("Directory where the file was: {:?}", &split[1]);
+            let mut reg_vec_decimal : Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
+            let reg_hex = vec_u8_to_vec_hex(reg_vec_decimal);
+
+            let mut contador = 0;
+            let mut aux = String::new().to_owned();
+            let mut hex_split: Vec<String> = Vec::new();
+
+            for i in reg_hex {
+                
+                if i.eq("00") {
+                    contador = contador + 1;
+                }
+                else {
+                    contador = 0;
+                }
+                
+                if !i.eq("00") {
+                    aux.push_str(&i);
+                }
+
+                if contador == 3 {
+                    hex_split.push(aux.to_owned());
+                    aux.clear();
+                }
+            }
+
+            // NOMBRE DEL PROGRAMA UTILIZADO
+            let program_name = parse_hex_string_to_ascii_string(&hex_split[0]);
+            println!("{:?}", program_name);
+            println!("{:?}", hex_split);
+            
+            
         }
     }
     return Ok(());
 }
 
-fn open_save_pid_mru() -> ForensicResult<()> {
-    let mut registry  = frnsc_liveregistry_rs::LiveRegistryReader{};
+fn vec_u8_to_vec_hex(value : Vec<u8>) -> Vec<String> {
 
-    let reg_key = registry.open_key
-    (forensic_rs::prelude::RegHiveKey::HkeyCurrentUser,"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSavePidlMRU\\*")?; 
-    //Abrimos la clave
-
-    let reg_values = registry.enumerate_values(reg_key)?; 
-    // Enumeramos todos los valores
-    
-    for top in reg_values {
-        println!("------------ {}", top);
-        let value : Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
-        let vec = vec_u8_to_vec_char(value);
-        let strin: String = vec.iter().collect::<String>();
-        println!("{}", strin);
-    }
-    return Ok(());
-}
-
-fn vec_u8_to_vec_char(value : Vec<u8>) -> Vec<char> {
-
-    let mut vec : Vec<char> = Vec::new();
+    let mut vec : Vec<String> = Vec::new();
 
     for i in value {
-        if i >= 32 && i <= 126 {
-            vec.push(i as char);
-        }    
+        let hex = format!("{:02X}", i);
+        vec.push(hex);
     }
 
     return vec;
+}
+
+fn parse_hex_string_to_ascii_string (hex_string : &String) -> String {
+
+    let subs = hex_string.as_bytes()
+                        .chunks(2)
+                        .map(str::from_utf8)
+                        .collect::<Result<Vec<&str>, _>>()
+                        .unwrap();
+
+    let mut aux = String::new().to_owned();
+
+    for y in subs {
+        let char = u8::from_str_radix(y, 16).map(|n| n as char).unwrap();
+        aux.push(char);
+    }
+
+    return aux;
+
 }
 
