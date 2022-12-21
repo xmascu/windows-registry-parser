520af47f72e15bb6f37c9b9e6427037534a0c88b
xmascu <119685592+xmascu@users.noreply.github.com>
2022-12-21 15:40:29 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,61 @@
-use std::{u8,str};
-use forensic_rs::prelude::{RegistryReader, ForensicResult};
+#![allow(non_snake_case)]
+
+use forensic_rs::prelude::{ForensicResult, RegistryReader};
+use std::u8;
 
 fn main() {
     let _ = last_visited_pid_mru().unwrap();
 }
 
 fn last_visited_pid_mru() -> ForensicResult<()> {
-    let mut registry  = frnsc_liveregistry_rs::LiveRegistryReader{};
+    let mut registry = frnsc_liveregistry_rs::LiveRegistryReader {};
 
-    let reg_key = registry.open_key
-    (forensic_rs::prelude::RegHiveKey::HkeyCurrentUser,"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\LastVisitedPidlMRU")?;
+    let reg_key = registry.open_key(
+        forensic_rs::prelude::RegHiveKey::HkeyCurrentUser,
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\LastVisitedPidlMRU",
+    )?;
     //Abrimos la clave
 
-    let reg_values = registry.enumerate_values(reg_key)?; 
+    let reg_values = registry.enumerate_values(reg_key)?;
     // Enumeramos todos los valores
-    
+
     for top in reg_values {
-        println!("------------ {}", top);
-        if !top.eq("MRUListEx") {
-            let mut reg_vec_decimal : Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
-            let reg_hex = vec_u8_to_vec_hex(reg_vec_decimal);
+        if top.eq("MRUListEx") { } 
+        else {
+            println!("----------- {}", top);
 
-            let mut contador = 0;
-            let mut aux = String::new().to_owned();
-            let mut hex_split: Vec<String> = Vec::new();
+            let reg_vec_decimal: Vec<u8> = registry.read_value(reg_key, &top)?.try_into()?;
+            println!("{:?}", reg_vec_decimal);
 
-            for i in reg_hex {
-                
-                if i.eq("00") {
-                    contador = contador + 1;
-                }
-                else {
-                    contador = 0;
-                    aux.push_str(&i);
-                }
+            let len_program = len_slice(&reg_vec_decimal[..]);
 
-                if contador == 3 {
-                    hex_split.push(aux.to_owned());
-                    aux.clear();
-                }
-            }
 
-            // NOMBRE DEL PROGRAMA UTILIZADO
-            let program_name = parse_hex_string_to_ascii_string(&hex_split[0]);
-            println!("{:?}", program_name);
-            println!("{:?}", hex_split);
-            
-            
+            let program_name = get_program_name(&reg_vec_decimal[..len_program.unwrap()]);
+
+            println!("{}", program_name);
         }
     }
     return Ok(());
 }
 
-fn vec_u8_to_vec_hex(value : Vec<u8>) -> Vec<String> {
+fn len_slice(value: &[u8]) -> Option<usize> {
+    let max_length = value.len() - (value.len() % 2);
 
-    let mut vec : Vec<String> = Vec::new();
-
-    for i in value {
-        let hex = format!("{:02X}", i);
-        vec.push(hex);
+    for pos in (0..max_length).step_by(2) {
+        if value[pos] == 0 && value[pos + 1] == 0 {
+            return Some(pos);
+        }
     }
-
-    return vec;
+    None
 }
 
-fn parse_hex_string_to_ascii_string (hex_string : &String) -> String {
+fn get_program_name(value: &[u8]) -> String {
+    let mut unicode_string = String::new();
 
-    let subs = hex_string.as_bytes()
-                        .chunks(2)
-                        .map(str::from_utf8)
-                        .collect::<Result<Vec<&str>, _>>()
-                        .unwrap();
-
-    let mut aux = String::new().to_owned();
-
-    for y in subs {
-        let char = u8::from_str_radix(y, 16).map(|n| n as char).unwrap();
-        aux.push(char);
+    for u8_value in value.iter() {
+        let unicode_char: char = *u8_value as char;
+        unicode_string.push(unicode_char);
     }
 
-    return aux;
-
+    return unicode_string;
 }
-
